6ba122e1f1fd49c153ab7001e403d75b216d9148
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-02-20 17:39:05 +0300

    Initial Commit

---
+++ CountryCell/CollectionViewCountryCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCountryCell.swift
+//  CountryCell
+//
+//  Created by Esna nur Yılmaz on 20.02.2024.
+//
+
+import UIKit
+
+class CollectionViewCountryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellLabel: UILabel!
+    
+}
---
+++ CountryCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryCell
+//
+//  Created by Esna nur Yılmaz on 20.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
